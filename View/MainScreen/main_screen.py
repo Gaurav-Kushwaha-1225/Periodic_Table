086f5a06ae0459aad57d7669468da8189f3b6e27
Gaurav Kushwaha <117214772+Gaurav-Kushwaha-1225@users.noreply.github.com>
2023-07-25 23:21:27 +0530

    Whole Project in MVC Framework

---
+++ View/MainScreen/main_screen.py
@@ -0,0 +1,10 @@
+from View.base_screen import BaseScreenView
+
+
+class MainScreenView(BaseScreenView):
+    def model_is_changed(self) -> None:
+        """
+        Called whenever any change has occurred in the data model.
+        The view in this method tracks these changes and updates the UI
+        according to these changes.
+        """
